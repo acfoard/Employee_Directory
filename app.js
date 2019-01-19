e00060ee855dda9fc92abdc32a5a2c50f47daecd
acfoard <acfoard@gmail.com>
2019-01-19 12:49:39 -0500

    working on verification

--- app.js
+++ app.js
@@ -1,23 +1,56 @@
+//Function to show Add Employee Page
 const showAdd = function (event) {
     event.preventDefault();
-    $('#allPage').addClass('hidden');
+    $('section').addClass('hidden');
     $('#addPage').removeClass('hidden');
-};
-
-const renderList = function() {
-    event.preventDefault();
-    $('#addPage').addClass('hidden');
-    $('#allPage').removeClass('hidden');
     $('.empList').empty();
     for (let i=0; i<employeeList.length; i++){
-        $('.empList').append(`<p>${employeeList[i].name}<br>${employeeList[i].officeNum}<br>${employeeList[i].phoneNum}</p>`);
+        $('.empList').append(`<div class="card"><p class="card-body">${employeeList[i].name}<br>${employeeList[i].officeNum}<br>${employeeList[i].phoneNum}</p></div>`);
     }
 };
 
+//Function to show Employee List
+const showAll = function() {
+    event.preventDefault();
+    $('section').addClass('hidden');
+    $('#allPage').removeClass('hidden');
+    $('.empList').empty();
+    for (let i=0; i<employeeList.length; i++){
+        $('.empList').append(`<div class="card"><p class="card-body">${employeeList[i].name}<br>${employeeList[i].officeNum}<br>${employeeList[i].phoneNum}</p></div>`);
+    }
+};
+
+//Function to show Verify Employee Page
+const showVerify = function(){
+    event.preventDefault();
+    $('section').addClass('hidden');
+    $('#verifyPage').removeClass('hidden');
+}
+
+//Function to verify name
+const verify = function() {
+    $(".result").empty();
+    const verName = $("#verifyName").val();
+    for (i=0; i < employeeList.length; i++){
+        if (employeeList[i].name.includes(verName)) {
+            $('.result').html("yes");
+            break;
+        } else {
+            console.log("no");
+        }
+    };    
+}
+
+//Initial Load - all pages hidden
 $('section').addClass('hidden');
 
-$('#showAll').on('click', renderList);
+//Navigation to each individual page
+$('#showAll').on('click', showAll);
 $('#showAdd').on('click', showAdd);
+$('#showVerify').on('click', showVerify);
+
+//Trigger Verificaiton
+$('#checkName').on('click', verify);
 
 
 
